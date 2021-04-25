3ef0001f73b6a834268e9b9d9be8f05980f9ca09
Davi <davi.silva41@fatec.sp.gov.br>
2021-04-25 18:56:55 -0300

    commit,I created three new classes for the course "JAVA FULL COURSE" Concept learned: IF Statements Switches Logical operators
    create Package "Exercise"
    create exercise Calculator.java but it don't work

--- src/Excercise/Calculator.java
+++ src/Excercise/Calculator.java
@@ -11,35 +11,35 @@ Calculator: simple calculator using CLI interface
 public class Calculator {
     public static void main(String[] args) {
         //variables
-//        int n1 = 0, n2= 0;
-//        long result = 0;
-//
-//
-//        while ("5".equals(operator)) {
-//            Scanner operator = new Scanner(System.in);
-//            System.out.println("1 (addition)  2 (subtraction)  3 (multiplication) 4 (division) 5 exit ");
-//            operator.nextInt();
-//            Scanner number = new Scanner(System.in);
-//
-//            if ("1".equals(operator)) {
-//
-//                n1 = number.nextInt();
-//
-//                n2 = number.nextInt();
-//                result = n1 + n2;
-//                System.out.println(n1 + " + " + n2 + " = ");
-//                System.out.println(result);
-//
-//            } else if ("2".equals(operator)) {
-//                n1 = number.nextInt();
-//                n2 = number.nextInt();
-//                result = n1 - n2;
-//                System.out.println(n1 + " - " + n2 + " = ");
-//                System.out.println(result);
-//
-//            } else {
-//                System.out.println("not operator please type an operator");
-//            }
-//        }
+
+      long result = 0;
+
+      int n1 = 0, n2= 0;
+      while ("5".equals(operator)) {
+          Scanner operator = new Scanner(System.in);
+          System.out.println("1 (addition)  2 (subtraction)  3 (multiplication) 4 (division) 5 exit ");
+          operator.nextInt();
+          Scanner number = new Scanner(System.in);
+
+          if ("1".equals(operator)) {
+
+              n1 = number.nextInt();
+
+              n2 = number.nextInt();
+              result = n1 + n2;
+              System.out.println(n1 + " + " + n2 + " = ");
+              System.out.println(result);
+
+          } else if ("2".equals(operator)) {
+              n1 = number.nextInt();
+              n2 = number.nextInt();
+              result = n1 - n2;
+              System.out.println(n1 + " - " + n2 + " = ");
+              System.out.println(result);
+
+          } else {
+              System.out.println("not operator please type an operator");
+          }
+        }
     }
 }
